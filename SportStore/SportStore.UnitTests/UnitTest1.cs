5c807eab187f624ac20756f5eda495c378bb476b
Maxim Myasnenko <ChipenkoMaxim1@gmail.com>
2021-02-15 18:38:03 +0700

    Add a taget gererator for each page

--- SportStore/SportStore.UnitTests/UnitTest1.cs
+++ SportStore/SportStore.UnitTests/UnitTest1.cs
@@ -4,9 +4,12 @@ using SportStore.Domain.Abstract;
 using SportStore.Domain.Entities;
 using System.Linq;
 using SportStore.WebUI.Controllers;
+using System.Web.Mvc;
+using SportStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SportStore.WebUI.HtmlHelpers;
 
 namespace SportStore.UnitTests
 {
@@ -35,6 +38,27 @@ namespace SportStore.UnitTests
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            HtmlHelper myHelper = null;
+
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<Int32, String> pageUrlDelegate = i => "Page" + i;
+
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a>" +
+                @"<a class=""selected"" href=""Page2"">2</a>" +
+                @"<a href=""Page3"">3</a>");
         }
     }
 }
