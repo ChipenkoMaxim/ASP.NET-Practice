1c3aa3ac92bdfdf2a2aa0c1f664349463b87f022
Maxim Myasnenko <ChipenkoMaxim1@gmail.com>
2021-02-15 09:46:01 +0700

    Add ProductController and ProductView, changes default routing

---
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        public ProductController(IProductsRepository productsRepository)
+        {
+            this.repository = productsRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
