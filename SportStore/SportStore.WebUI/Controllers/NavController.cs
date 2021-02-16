e77be8e26e0cefa07884e9c0eefd083cdb7d112d
Maxim Myasnenko <ChipenkoMaxim1@gmail.com>
2021-02-16 14:49:55 +0700

    Add category navigation

---
+++ SportStore/SportStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Abstract;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IProductsRepository repository;
+        public NavController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(String category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<String> categories = repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
